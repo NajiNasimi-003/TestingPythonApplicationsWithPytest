eda26d9ea13bae31305bdfec240f645a5f5b8ab7
NajiNasimi-003 <77068502+NajiNasimi-003@users.noreply.github.com>
2022-06-14 03:26:47 -0700

    Final

--- pytest_project/test_wallet.py
+++ pytest_project/test_wallet.py
@@ -14,6 +14,11 @@ def wallet():
     '''Returns a Wallet instance with a balance of 20'''
     return Wallet(20)
 
+@pytest.fixture
+def my_wallet():
+    '''Returns a Wallet instance with a zero balance'''
+    return Wallet()
+
 def test_default_initial_amount(empty_wallet):
     assert empty_wallet.balance == 0
 
@@ -31,3 +36,16 @@ def test_wallet_spend_cash(wallet):
 def test_wallet_spend_cash_raises_exception_on_insufficient_amount(empty_wallet):
     with pytest.raises(InsufficientAmount):
         empty_wallet.spend_cash(100)
+<<<<<<< Updated upstream
+=======
+
+
+@pytest.mark.parametrize("earned,spent,expected", [
+    (30, 10, 20),
+    (20, 2, 18),
+])
+def test_transactions(earned, spent, expected):
+    my_wallet.add_cash(earned)
+    my_wallet.spend_cash(spent)
+    assert my_wallet.balance == expected
+>>>>>>> Stashed changes
